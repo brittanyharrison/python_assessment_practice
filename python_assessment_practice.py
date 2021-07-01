ccf1768fb82526e89786fe3216b3063cab7e8881
Brittany Harrison <bkharrison94@gmail.com>
2021-07-01 10:25:12 +0100

    Q2 ADDED

--- python_assessment_practice.py
+++ python_assessment_practice.py
@@ -4,9 +4,15 @@ Sample questions to help with the python assessment.
 
 
 # Declare a function called username that takes one arg as a string and return the name
-def username(name="Brittany"):
-    return name
+# def username(name):
+#     return name
+#
+#
+# print(username("Brittany"))
 
+# Declare a list with numbers 1-5. Iterate through the list and display the list
+# this_list = [1, 2, 3, 4, 5]
+# for number in list:
+#     print(number)
 
-print(username())
 #
